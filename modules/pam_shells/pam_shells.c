d8eba606078c57e5480a82b5c1a1c9df4cf83a01
Thorsten Kukuk <kukuk@thkukuk.de>
2003-11-25 09:06:54 +0000

    Relevant BUGIDs: 848337
    Purpose of commit: bugfix
    
    Commit summary:
    ---------------
    pam_shells uses strlen/strcmp, but does not
     include the corresponding string.h header file.
     This could lead to problems on architectures,
     where gcc needs a correct prototype to generate
     correct calling code.
    
     pam_stress.c complains about a redefined
     __USE_BSD symbol. This is a internal glibc
     define and should not be used in source code.

--- modules/pam_shells/pam_shells.c
+++ modules/pam_shells/pam_shells.c
@@ -12,6 +12,7 @@
 
 #include <pwd.h>
 #include <stdarg.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
--- modules/pam_stress/pam_stress.c
+++ modules/pam_stress/pam_stress.c
@@ -10,7 +10,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define __USE_BSD
 #include <syslog.h>
 
 #include <stdarg.h>
