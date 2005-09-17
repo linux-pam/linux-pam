1574ef8c3088242bb3c88d45fcf17e642bb60403
Tomas Mraz <tm@t8m.info>
2005-09-17 08:59:04 +0000

    Relevant BUGIDs:
    Purpose of commit: cleanup
    
    Commit summary:
    ---------------
    Suppress unused parameters warning (by ldv)

--- modules/pam_warn/pam_warn.c
+++ modules/pam_warn/pam_warn.c
@@ -81,23 +81,24 @@ int pam_sm_chauthtok(pam_handle_t *pamh, int flags UNUSED,
 }
 
 PAM_EXTERN int
-pam_sm_acct_mgmt (pam_handle_t *pamh, int flags, int argc, const char **argv)
+pam_sm_acct_mgmt(pam_handle_t *pamh, int flags UNUSED,
+		 int argc UNUSED, const char **argv UNUSED)
 {
     log_items(pamh, __FUNCTION__);
     return PAM_IGNORE;
 }
 
 PAM_EXTERN int
-pam_sm_open_session (pam_handle_t *pamh, int flags, int argc,
-                     const char **argv)
+pam_sm_open_session(pam_handle_t *pamh, int flags UNUSED,
+		    int argc UNUSED, const char **argv UNUSED)
 {
     log_items(pamh, __FUNCTION__);
     return PAM_IGNORE;
 }
 
 PAM_EXTERN int
-pam_sm_close_session (pam_handle_t *pamh, int flags, int argc,
-		      const char **argv)
+pam_sm_close_session(pam_handle_t *pamh, int flags UNUSED,
+		     int argc UNUSED, const char **argv UNUSED)
 {
     log_items(pamh, __FUNCTION__);
     return PAM_IGNORE;
