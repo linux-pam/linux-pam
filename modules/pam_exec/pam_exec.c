02bb68f7d71b19461087a51f872a21cf682bd608
Dmitry V. Levin <ldv@altlinux.org>
2006-08-28 23:02:58 +0000

    Relevant BUGIDs:
    Purpose of commit: bugfix
    
    Commit summary:
    ---------------
    
    2006-08-29  Dmitry V. Levin  <ldv@altlinux.org>
    
    	* modules/pam_exec/pam_exec.c (call_exec): Add required third
    	argument to open() call with O_CREAT flag set.

--- modules/pam_exec/pam_exec.c
+++ modules/pam_exec/pam_exec.c
@@ -159,7 +159,8 @@ call_exec (pam_handle_t *pamh, int argc, const char **argv)
 	  time_t tm = time (NULL);
 	  char *buffer = NULL;
 
-	  if ((i = open (logfile, O_CREAT|O_APPEND|O_WRONLY))  == -1)
+	  if ((i = open (logfile, O_CREAT|O_APPEND|O_WRONLY,
+	  		 S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)) == -1)
 	    {
 	      int err = errno;
 	      pam_syslog (pamh, LOG_ERR, "open of %s failed: %m",
