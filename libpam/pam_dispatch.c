4e53d8d8c64e89a05c24e4a208675f28680f7aa7
Thorsten Kukuk <kukuk@thkukuk.de>
2009-02-17 16:34:47 +0000

    Relevant BUGIDs: bugzilla.novell.com#470337
    Purpose of commit: bugfix
    
    Commit summary:
    ---------------
    
    2009-02-17  Thorsten Kukuk  <kukuk@thkukuk.de>
    
            * doc/man/pam_sm_chauthtok.3.xml: Document that sufficient
            can break the PRELIM_CHECK chain.
    
            * libpam/pam_dispatch.c: Don't freeze chain for chauthtok
            [bugzilla.novell.com#470337]

--- libpam/pam_dispatch.c
+++ libpam/pam_dispatch.c
@@ -132,11 +132,10 @@ static int _pam_dispatch_aux(pam_handle_t *pamh, int flags, struct handler *h,
 	}
 
 	/*
-	 * use_cached_chain is how we ensure that the setcred/close_session
-	 * and chauthtok(2) modules are called in the same order as they did
-	 * when they were invoked as auth/open_session/chauthtok(1). This
-	 * feature was added in 0.75 to make the behavior of pam_setcred
-	 * sane. It was debugged by release 0.76.
+	 * use_cached_chain is how we ensure that the setcred and
+         * close_session modules are called in the same order as they did
+	 * when they were invoked as auth/open_session. This feature was
+	 * added in 0.75 to make the behavior of pam_setcred sane.
 	 */
 	if (use_cached_chain != _PAM_PLEASE_FREEZE) {
 
@@ -358,9 +357,6 @@ int _pam_dispatch(pam_handle_t *pamh, int flags, int choice)
 	break;
     case PAM_CHAUTHTOK:
 	h = pamh->handlers.conf.chauthtok;
-	if (flags & PAM_UPDATE_AUTHTOK) {
-	    use_cached_chain = _PAM_MUST_BE_FROZEN;
-	}
 	break;
     default:
 	pam_syslog(pamh, LOG_ERR, "undefined fn choice; %d", choice);
