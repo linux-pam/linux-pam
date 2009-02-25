6e40e2920874b268008be1cab4ee2565d5615658
Thorsten Kukuk <kukuk@thkukuk.de>
2009-02-25 15:59:45 +0000

    Relevant BUGIDs:
    Purpose of commit: bugfix
    
    Commit summary:
    ---------------
    
    2009-02-25  Thorsten Kukuk  <kukuk@thkukuk.de>
    
            * libpam/pam_misc.c (_pam_StrTok): Use unsigned char
            instead of int. Reported by Marcus Granado.
            * tests/Makefile.am (TESTS): Add tst-pam_mkargv.
            * tests/tst-pam_mkargv.c (main): Test case for
            _pam_mkargv.

---
+++ tests/tst-pam_mkargv.c
@@ -0,0 +1,51 @@
+/*
+   Copyright (C) Thorsten Kukuk <kukuk@suse.de> 2009
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation in version 2 of the License.
+*/
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <stdio.h>
+
+#include "libpam/pam_misc.c"
+
+/* Simple program to see if _pam_mkargv() would succeed. */
+int main(void)
+{
+  char *argvstring = "user = XENDT\\userα user=XENDT\\user1";
+  const char *argvresult[] = {"user", "=", "XENDT\\userα",
+			      "user=XENDT\\user1"};
+  int myargc;
+  char **myargv;
+  int argvlen;
+  int i;
+
+  argvlen = _pam_mkargv(argvstring, &myargv, &myargc);
+
+#if 0
+  printf ("argvlen=%i, argc=%i", argvlen, myargc);
+  for (i = 0; i < myargc; i++) {
+    printf(", argv[%d]=%s", i, myargv[i]);
+  }
+  printf ("\n");
+#endif
+
+  if (argvlen != 333)
+    return 1;
+
+  if (myargc != 4)
+    return 1;
+
+  for (i = 0; i < 4; i++)
+    {
+      if (strcmp (myargv[i], argvresult[i]) != 0)
+	return 1;
+    }
+
+  return 0;
+}
