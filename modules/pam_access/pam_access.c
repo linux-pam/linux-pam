ac39f12251d058cd080940d260dd4a860d79c4bf
FIRST_NAME LAST_NAME <pschou@users.noreply.github.com>
2022-12-08 20:50:58 -0500

    add pam_limit_ssh

--- modules/pam_access/pam_access.c
+++ modules/pam_access/pam_access.c
@@ -1082,13 +1082,14 @@ pam_sm_authenticate (pam_handle_t *pamh, int flags UNUSED,
     if (rv == NOMATCH && loginfo.config_file == default_config) {
         char **filename_list = read_access_dir(pamh);
         if (filename_list != NULL) {
-            for (int i = 0; filename_list[i] != NULL; i++) {
+            int i;
+            for (i = 0; filename_list[i] != NULL; i++) {
                 loginfo.config_file = filename_list[i];
                 rv = login_access(pamh, &loginfo);
                 if (rv != NOMATCH)
                     break;
             }
-            for (int i = 0; filename_list[i] != NULL; i++)
+            for (i = 0; filename_list[i] != NULL; i++)
                 free(filename_list[i]);
             free(filename_list);
         }
---
+++ modules/pam_limit_ssh/pam_limit_ssh.c
@@ -0,0 +1,157 @@
+/*
+ * pam_limit_ssh module
+ *
+ * Written by Paul Schou <github.com/pschou> 2022/12/8
+ */
+
+#include <syslog.h>
+#include <glob.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+
+/* SSHD processes from globbed exe links. */
+#define PROC_GLOB            "/proc/*/exe"
+#define PROC_CMDLINE         "cmdline"
+#define PROC_CMDLINE_PREFIX  "sshd: "
+#define PROC_CMDLINE_SUFFIX  "@"
+
+/* --- authentication management functions --- */
+
+int
+pam_sm_authenticate(pam_handle_t *pamh UNUSED, int flags UNUSED,
+        int argc UNUSED, const char **argv UNUSED)
+{
+    if (auth(pamh, flags, argc, argv) == PAM_SUCCESS)
+        return PAM_SUCCESS;
+    return PAM_AUTH_ERR;
+}
+
+int
+pam_sm_setcred(pam_handle_t *pamh UNUSED, int flags UNUSED,
+         int argc UNUSED, const char **argv UNUSED)
+{
+    if (auth(pamh, flags, argc, argv) == PAM_SUCCESS)
+        return PAM_SUCCESS;
+    return PAM_CRED_ERR;
+}
+
+/* --- account management functions --- */
+
+int
+pam_sm_acct_mgmt(pam_handle_t *pamh UNUSED, int flags UNUSED,
+     int argc UNUSED, const char **argv UNUSED)
+{
+    if (auth(pamh, flags, argc, argv) == PAM_SUCCESS)
+        return PAM_SUCCESS;
+    return PAM_AUTH_ERR;
+}
+
+/* --- password management --- */
+
+int
+pam_sm_chauthtok(pam_handle_t *pamh UNUSED, int flags UNUSED,
+     int argc UNUSED, const char **argv UNUSED)
+{
+    if (auth(pamh, flags, argc, argv) == PAM_SUCCESS)
+        return PAM_SUCCESS;
+    return PAM_AUTHTOK_ERR;
+}
+
+/* --- session management --- */
+
+int
+pam_sm_open_session(pam_handle_t *pamh UNUSED, int flags UNUSED,
+        int argc UNUSED, const char **argv UNUSED)
+{
+    if (auth(pamh, flags, argc, argv) == PAM_SUCCESS)
+        return PAM_SUCCESS;
+    return PAM_SESSION_ERR;
+}
+
+int
+pam_sm_close_session(pam_handle_t *pamh UNUSED, int flags UNUSED,
+         int argc UNUSED, const char **argv UNUSED)
+{
+    return PAM_SUCCESS;
+}
+
+/* end of module definition */
+
+int
+auth( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
+    const char* user;
+    int retval;
+    // parse the user
+    retval = pam_get_user(pamh, &user, NULL);
+
+    glob_t globbuf;
+    int glob_rv = glob(PROC_GLOB, GLOB_ERR | GLOB_NOSORT, NULL, &globbuf);
+    char buf[2048];
+    ssize_t buf_len;
+    FILE *cmdfile;
+    const char *sshd_path;
+
+    int i, ssh_count, ssh_max;
+    ssh_max = 10;
+    sshd_path = "/usr/sbin/sshd";
+
+    // parse the args to find max and sshd process
+    for (; argc-- > 0; ++argv) {
+        if (strncmp(*argv, "max=", 4) == 0) {
+            i = atoi(*argv+4);
+            if (i > 0)
+                ssh_max = i;
+        } else if (strncmp(*argv, "sshd=", 4) == 0) {
+            sshd_path = *argv + 5;
+        }
+    }
+
+    ssh_count = 0;
+    if (glob_rv == 0) {
+        for (i = 0; i < globbuf.gl_pathc; i++) {
+            // step through the /proc/#/exe list
+            buf_len = readlink(globbuf.gl_pathv[i], buf, sizeof(buf)-1);
+            if (buf_len == -1) { continue; }
+
+            buf[buf_len] = '\0';
+            if (strcmp(buf, sshd_path) == 0) {
+                // build /proc/#/cmdline filename from the /proc/#/exe path
+                for (buf_len = 0; buf_len < 1016 && globbuf.gl_pathv[i][buf_len] != 0; buf_len++) {
+                    buf[buf_len] = globbuf.gl_pathv[i][buf_len];
+                }
+                strncpy(buf+buf_len-3, PROC_CMDLINE, 8);
+                
+                // open the file and read it in
+                cmdfile = fopen(buf, "r");
+                if(cmdfile == NULL) // continue if file is not readable
+                    continue;
+                fread(buf, sizeof(char), sizeof(buf), cmdfile);
+                fclose(cmdfile);
+
+                // compare the content with the PREFIX, USER, SUFFIX
+                buf_len = strlen(PROC_CMDLINE_PREFIX);
+                if (strncmp(buf, PROC_CMDLINE_PREFIX, buf_len) != 0)
+                    continue;
+                if (strncmp(buf+buf_len, user, strlen(user)) != 0)
+                    continue;
+                buf_len = buf_len + strlen(user);
+                if (strncmp(buf+buf_len, PROC_CMDLINE_SUFFIX, strlen(PROC_CMDLINE_SUFFIX)) != 0)
+                    continue;
+
+                // count the session
+                ssh_count++;
+            }
+        }
+        pam_syslog(pamh, LOG_NOTICE, "user %s, current %d, max %d", user, ssh_count, ssh_max);
+
+        if (ssh_count >= ssh_max) {
+            return PAM_AUTH_ERR;
+        }
+    }
+    return PAM_SUCCESS;
+}
