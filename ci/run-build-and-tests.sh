086745547df419ae5ab96258ab82053d19fb803c
Dmitry V. Levin <ldv@strace.io>
2023-04-04 08:00:00 +0000

    ci: add --enable-openssl jobs to the ci matrix
    Link: https://github.com/linux-pam/linux-pam/pull/550#issuecomment-1490362439

--- ci/run-build-and-tests.sh
+++ ci/run-build-and-tests.sh
@@ -38,6 +38,12 @@ case "${VENDORDIR-}" in
 		;;
 esac
 
+case "${USE_OPENSSL-}" in
+	yes)
+		DISTCHECK_CONFIGURE_FLAGS="$DISTCHECK_CONFIGURE_FLAGS --enable-openssl"
+		;;
+esac
+
 echo 'BEGIN OF BUILD ENVIRONMENT INFORMATION'
 uname -a |head -1
 libc="$(ldd /bin/sh |sed -n 's|^[^/]*\(/[^ ]*/libc\.so[^ ]*\).*|\1|p' |head -1)"
